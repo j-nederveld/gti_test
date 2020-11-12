56dd59fa6fa1c4bc20a0ad4fe08c8dbdf8effb70
John Nederveld <john.nederveld@gmail.com>
2020-11-12 11:24:54 -0500

    final cleanup and push

--- public/assets/scripts/convertTimeStamp.js
+++ public/assets/scripts/convertTimeStamp.js
@@ -1,25 +1,25 @@
 function convertTimeStamp(timestamp) {
-    var a = new Date(timestamp * 1000);
-    var months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var day = a.getDate();
-    var time = day + " " + month + " " + year;
-    console.log(time);
-    return time;
-  }
+  var a = new Date(timestamp * 1000);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var day = a.getDate();
+  var time = day + " " + month + " " + year;
+  console.log(time);
+  return time;
+}
 
-  module.exports = convertTimeStamp;
+module.exports = convertTimeStamp;
