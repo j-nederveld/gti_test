2d1248f5631de4200566457957ec9b4d09c676e7
John Nederveld <john.nederveld@gmail.com>
2020-11-12 11:21:48 -0500

    formatting

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,6 +1,6 @@
-const convertTimeStamp = require("../public/assets/scripts/convertTimeStamp")
+const convertTimeStamp = require("../public/assets/scripts/convertTimeStamp");
 
 test("Can instantiate Employee instance", () => {
-    const timestamp = 1604525340;
-    expect(convertTimeStamp(timestamp)).toBe("4 Nov 2020")
-  });
+  const timestamp = 1604525340;
+  expect(convertTimeStamp(timestamp)).toBe("4 Nov 2020");
+});
